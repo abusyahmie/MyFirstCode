60d1e0bdd709ed53fe220e380b63006d985e7f39
Ahmad Shahril Akmar Ahmad Rashdi <abusyahmie@gmail.com>
2015-04-15 18:23:25 +0800

    Initial Commit

---
+++ MyFirstCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyFirstCode
+//
+//  Created by AHMAD SHAHRIL AKMAR AHMAD RASHDI on 4/15/15.
+//  Copyright (c) 2015 AHMAD SHAHRIL AKMAR AHMAD RASHDI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
